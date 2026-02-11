48e57f8edd0079762c3a6cef33eda4a4a962e85a
Daniel Persson <wavezodium@gmail.com>
2026-02-11 12:28:45 +0100

    Add Models and DB's to project.

---
+++ ChalkboardChat.DAL/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using ChalkboardChat.DAL.Models;
+
+namespace ChalkboardChat.DAL.Data;
+
+public class AppDbContext : DbContext {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+    }
+
+    public DbSet<MessageModel> Messages { get; set; }
+}
