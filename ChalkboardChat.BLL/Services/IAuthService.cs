c7c44ed73849feb18815a278956ca2c52978a5cb
Robert Österberg <rosterberg93@gmail.com>
2026-02-11 12:30:19 +0100

    Added IAuthService

---
+++ ChalkboardChat.BLL/Services/IAuthService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace ChalkboardChat.BLL.Services
+{
+    public interface IAuthService
+    {
+        Task<User?> RegisterAsync (string username, string password);
+        Task<User?> LoginAsync (string username, string password);
+        Task<bool> ChangeUsernameAsync(int userId, string newUsername);
+        Task<bool> ChangePasswordAsync(int userId, string currentPassword, string newPassword);
+        Task<bool> DeleteAccountAsync(int userId, string password);
+
+
+    }
+}
+
