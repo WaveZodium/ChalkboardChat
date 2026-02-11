148fb78d89b3fbdec4b3d2575d67ba58000b9b2c
WaveZodium <wavezodium@gmail.com>
2026-02-11 16:56:35 +0100

    Update migrations.

---
+++ ChalkboardChat.DAL/Migrations/20260211153153_UpdateAppModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChalkboardChat.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAppModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Messages",
+                newName: "UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "Message",
+                table: "Messages",
+                newName: "Text");
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Messages",
+                newName: "CreatedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Messages",
+                newName: "Username");
+
+            migrationBuilder.RenameColumn(
+                name: "Text",
+                table: "Messages",
+                newName: "Message");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Messages",
+                newName: "Date");
+        }
+    }
+}
