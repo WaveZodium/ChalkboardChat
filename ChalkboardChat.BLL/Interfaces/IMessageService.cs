017e20a8933eab0129b8ae9a5162eac66fb71dc0
Abbaskhudayer <abbas.khudayer@hotmail.com>
2026-02-11 23:58:29 +0100

    Implementerat MessageService med affärslogik för meddelanden
    - Skapat MessageService.cs som implementerar IMessageService
    - Validerar tomma meddelanden och userId i PostMessageAsync
    - Hanterar deleted user-logik i GetAllMessagesAsync
    - Sorterar meddelanden nyast först
    - Använder UserManager för dynamisk användarnamn-hämtning

---
+++ ChalkboardChat.BLL/Interfaces/IMessageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChalkboardChat.BLL.DTOs;
+
+namespace ChalkboardChat.BLL.Interfaces;
+
+//kontrakt för ui - ui använder detta
+public interface IMessageService
+{
+    //posta nytt meddelande
+    Task<bool> PostMessageAsync(CreateMessageDto dto);
+    
+    //hämta alla meddelanden för visning
+    Task<List<MessageDto>> GetAllMessagesAsync(string currentUserId);
+}
