7f281505068fae0f0dd6217261b0548c13c42476
Ditt Namn <linda-maria.m@hotmail.com>
2026-02-16 16:07:38 +0100

    Start (meddelandesida): Finjusterat vyn. Lagt till krav på auktorisering. DAL/BLL: Behöver att Meddelande-modellen innehåller string UserName. Saknas metoder "GetAllMessagesAsync". och "AddMessageAsync" i IMessageService?

--- ChalkboardChat.UI/Pages/Member/Start.cshtml.cs
+++ ChalkboardChat.UI/Pages/Member/Start.cshtml.cs
@@ -4,23 +4,26 @@ using ChalkboardChat.DAL.Models;
 using ChalkboardChat.BLL.Services;
 using Microsoft.AspNetCore.Mvc;
 using ChalkboardChat.DAL;
+using Microsoft.AspNetCore.Authorization;
+using ChalkboardChat.BLL.Interfaces;
 
 namespace ChalkboardChat.UI.Pages.Member
 {
     // En PageModel hanterar logiken för varje Razor Page (motsvarar en Controller i MVC-mönstret och ViewModel i MVVM-mönstret)
+    [Authorize] // NÖDVÄNDIGT för att skydda sidan så att endast inloggade användare kan se den
     public class StartModel : PageModel
     {
         // DEL 1 - MANAGERS och SERVICES 
         // Skapa relation till SignInManager för att hålla koll på vilken användare som är inloggad (GÖR DEN DET?) 
         private readonly SignInManager<IdentityUser> _signInManager;
         // Skapa relation till MessageService för att hämta meddelanden från databasen
-        private readonly MessageService _messageService;
+        private readonly IMessageService _iMessageService;
 
         // KONSTRUKTOR för modellen, tar managers och services som parametrar  
-        public StartModel(SignInManager<IdentityUser> signInManager, MessageService messageService)
+        public StartModel(SignInManager<IdentityUser> signInManager, IMessageService iMessageService)
         {
             _signInManager = signInManager;
-            _messageService = messageService;
+            _iMessageService = iMessageService;
         }
 
         // EGENSKAP för att hålla listan med meddelanden
@@ -30,7 +33,7 @@ namespace ChalkboardChat.UI.Pages.Member
         public async Task OnGetAsync()
         {
             // Hämta alla meddelanden från databasen och spara i lokal variabel "Messages"
-            Messages = await _messageService.GetAllMessagesAsync();
+            Messages = await _iMessageService.GetAllMessagesAsync();
         }
         // METOD för att skicka nytt meddelande till databas
         public async Task<IActionResult> OnPostAsync(string message)
@@ -39,7 +42,7 @@ namespace ChalkboardChat.UI.Pages.Member
             if (!string.IsNullOrWhiteSpace(message))
             {
                 // Spara det nya meddelandet i databasen
-                await _messageService.AddMessageAsync(message, 
+                await _iMessageService.AddMessageAsync(message, 
                     User.Identity!.Name);
             }
             // ... och omdirigera till sida för att visa meddelandelistan igen
